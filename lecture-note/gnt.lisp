3800f5075e827b9b8282c6cf2ca2d55e73cd3a14
SeungRhee Oh <sroh@SR5HMBA.local>
2020-09-13 14:49:24 +0900

    Update.

--- lecture-note/gnt.lisp
+++ lecture-note/gnt.lisp
@@ -4,14 +4,16 @@
 
 ;; args
 ;; ((:type functionp state1 state2 )..)
-(srl-search '(1) '#(labmda (s) (eq 15 s))
-		     #'append
-		     #'(lambda (states) (print (car states)))
-		     #'(lambda (s) (+ 1 s) s)
-		     #'car
-		     #'exist-in-group-p
-		     #'generate-new-states
-		     '())
+(srl-search '(1)
+	    #'(lambda (s) (eq 15 s))
+	    #'append
+	    #'(lambda (states) (print (car states)))
+	    #'(lambda (s) (list (+ 1 s) s))
+	    #'car
+	    #'exist-in-group-p
+	    #'identity
+	    #'generate-new-states
+	    '(eq 1 2 3) '(eq -1 -2 -3) '(eq 10 11 12) '(eq 17 18 19))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
 
@@ -23,27 +25,31 @@
 		   combine result successors selector group-fn post-process generate-new-states
 		   &rest args)
   "Generic search function."
-  (cond ((null states) (error "starved states."))
-	;; first of states is best select.
-	((funcall goalp (car states)) (funcall result states))
-	(t (let* ((candidate-states (funcall successors (funcall selector states))) ; TODO: need to here?/
-		  (filtered-state (apply (funcall generate-new-states candidate-states group-fn) args))
-		  (new-states (funcall combine
-				       ;; pick candidate-states satisfied group justifying
-				       ;; if need, post processing (ex. order filtered states
-				       (funcall post-process filtered-state)
-				       (cdr states))))
-	     (apply #'srl-search 
-		    new-states goalp combine result generate-new-states args)))))
-
-
+  (if (null states)
+      (error "starved states.")
+      (let ((state (funcall selector states)))
+	(if (funcall goalp (funcall selector states))
+	    (funcall result states)
+	    (let* ((candidate-states (funcall successors state))
+		   (filtered-state (apply (funcall generate-new-states
+						   candidate-states :group-fn group-fn)
+					  args))
+		   (new-states (funcall combine
+					;; pick candidate-states satisfied group justifying
+					;; if need, post processing (ex. order filtered states
+					(funcall post-process filtered-state)
+					(cdr states))))
+	      ;; TODO: how to treat current state?
+	      (apply #'srl-search 
+		     new-states goalp combine result
+		     successors selector group-fn post-process generate-new-states args))))))
 
 ;; reference args may be many, how to decide new-states's compare-function
 ;; as it is
 ;; group: ((c-fn1 . (s11 s12 s13 ..)) (c-fn2 . (s21 s22 s23 ..)))
 ;; selector choose one of states. (car states)
 ;; cf. when use &rest and &key parameter, use &allow-other-keys
-(defun generate-new-states (candidate-states &key (group-fn #'exist-in-group-p))
+(defun generate-new-states (candidate-states &key group-fn)
   "Cause of various data type, set how to apply group function, where group-fn in data."
   (lambda (&rest group)
     (apply #'%generate-new-states candidate-states group-fn group)))
@@ -54,13 +60,15 @@
 
 (defun exist-in-group-p (group)
   (labels ((iterate-states (fn s states)
-	     (cond ((null states) nil)
-		   (t (or (funcall fn s (car states)) (iterate-states fn s (cdr states))))))
+	     (if (null states)
+		 nil
+		 (or (funcall fn s (car states)) (iterate-states fn s (cdr states)))))
 	   (apply-each-group (s g)
 	     (iterate-states (car g) s (cdr g)))
 	   (iterate-group (s g)
-	     (cond ((null g) nil)
-		   (t (or (apply-each-group s (car g)) (iterate-group s (cdr g)))))))
+	     (if (null g)
+		 nil
+		 (or (apply-each-group s (car g)) (iterate-group s (cdr g))))))
     (lambda (s)
       (iterate-group s group))))
 
